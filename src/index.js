196b8dd299443f241c7bec9d140360a5b8a37b9c
Vlad Nemanikhin <77212359+Vlad-Nemanikhin@users.noreply.github.com>
2021-07-04 16:02:00 +0300

    Создал шаблон и подключил иконки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './sass/main.scss';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import tmp from "./templates/main.hbs";
+import menu from "./menu.json";
+import './sass/styles.css';
+
+console.log({ els: menu });
+console.log(tmp({ els: menu }));
+
+const listEl = document.querySelector('.menu js-menu');
+listEl.innerHTML = tmp({ els: menu });
