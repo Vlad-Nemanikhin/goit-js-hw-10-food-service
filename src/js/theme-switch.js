630666ec766c56f736690213f5340b3151ee9854
Vlad Nemanikhin <77212359+Vlad-Nemanikhin@users.noreply.github.com>
2021-07-06 14:39:30 +0300

    Испавил

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -4,44 +4,18 @@ const Theme = {
 };
 
 const themeEl = document.querySelector('#theme-switch-toggle');
-
-themeEl.addEventListener( 'change' , onChangeToggle);
+themeEl.addEventListener('change', onChangeToggle);
+localStorage.setItem('theme', Theme.LIGHT);
 
 function onChangeToggle(e) {
-	const checked = e.currentTarget.checked
-      if (checked) {
-         setDarkTheme();
-		}
-	
-      if (!checked) {
-			setLightTheme();
-      }
-}
-
-getCurrentTheme();
-
-function getCurrentTheme() {
-      const currentTheme = localStorage.getItem('theme');
-
-      if (currentTheme === Theme.LIGHT || currentTheme === null) {
-      	setLightTheme()
-      }
-
-      if (currentTheme === Theme.DARK) {
-         setDarkTheme()
-      }
+   const checked = e.currentTarget.checked;
+   if (checked) {
+      setDarkTheme();
+   } else { document.body.classList.remove(Theme.DARK); }
 }
 
 function setDarkTheme() {
 	document.body.classList.remove(Theme.LIGHT);
 	document.body.classList.add(Theme.DARK);
    localStorage.setItem('theme', Theme.DARK);
-   themeEl.checked === true
-}
-
-function setLightTheme() {
-   document.body.classList.remove(Theme.DARK);
-   document.body.classList.add(Theme.LIGHT);
-   localStorage.setItem('theme', Theme.LIGHT);
-   themeEl.checked === false
 }
